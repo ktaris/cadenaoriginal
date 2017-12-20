fe9ae78c01dd2444dcc14e739b0c8cfc28ada9e1
Carlos Ramos <carlos@ramoscarlos.com>
2017-12-19 23:04:05 -0700

    Commit inicial para generar cadena original con xslt.

---
+++ CadenaOriginal.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2017 Carlos Ramos
+ * @package ktaris-csd
+ * @version 0.1.0
+ */
+
+namespace ktaris\cadenaoriginal;
+
+class CadenaOriginal
+{
+    public static function obtener($xml)
+    {
+        // Se carga el xsl, en base al xslt original, con cadenas para incluir los archivos
+        // de los demás xslt desde archivos locales, a través de una sustitución de cadena
+        // con PHP, y se entrega ee archivo al processamiento de xslt posteriormente.
+        $basePath = dirname(__FILE__).DIRECTORY_SEPARATOR.'xslt';
+        $xslFileName = $basePath.DIRECTORY_SEPARATOR.'cadenaoriginal_3_3.xsl';
+        $xslFile = file_get_contents($xslFileName);
+        $xslFile = str_replace('{$PATH}', $basePath, $xslFile);
+
+        // XSL
+        $xsl = new \DOMDocument();
+        $xsl->loadXML($xslFile);
+
+        // CFDI
+        $cfdi = new \SimpleXMLElement($xml);
+
+        $xslt = new \XSLTProcessor();
+
+        // Realmente, no sé por qué al quitar el "@" tenemos la advertencia siguiente:
+        //     XSLTProcessor::importStylesheet(): compilation error: line 2 element stylesheet
+        // pero, la cadena original se genera correctamente si ignoramos tal advertencia.
+        @$xslt->importStylesheet($xsl);
+
+        $newdom = $xslt->transformToDoc($cfdi);
+
+        return $newdom->textContent;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# cadenaoriginal
+# Cadena Original
+
 Crea la cadena original en base a los XSLT liberados por el SAT y la cadena XML del CFDI.
+
+## Instalación
+
+La manera recomendada para instalar esta extensión es a través de composer:
+
+```
+php composer.phar require --prefer-dist ktaris/cadenaoriginal "dev-master"
+```
+
+O agregándolo directamente al archivo `composer.json`:
+
+```
+"ktaris/cadenaoriginal": "dev-master"
+```
+
+## Ejemplo de uso
+
+
+La clase `CadenaOriginal` solamente contiene un método estático que recibe una cadena del XML de un CFDI como argumento, y regresa una cadena de texto correspondiente a la cadena original.
+
+Para su uso, hacer lo siguiente:
+
+```
+use ktaris\cadenaoriginal\CadenaOriginal;
+
+$cadenaOriginal = CadenaOriginal::obtener($xml);
+```
